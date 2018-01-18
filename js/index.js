525f2ed30c2578a82ba3d71fc1cc29430ba68da6
MautK <maud.canisius@gmail.com>
2018-01-18 19:40:05 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,40 +5,7 @@
 //location variables these should in the end come directly from your phone/computer
 var me = [{ name: "maud", x: 11.316899, y: 50.979899 }];
 
-var friends = [ 
-  { name: "rachel", x: 11.332045, y: 50.971160 },
-  { name: "christoph", x: 11.315262, y: 50.974162 },
-  { name: "pablo", x: 11.32758, y: 50.973784 },
-  { name: "marta", x: 11.33009, y: 50.984816 },
-  { name: "alineWeimar", x: 11.316899, y: 50.979897 },
-  { name: "hauptbahnhof", x: 11.32771, y: 50.991164 },
-  { name: "belvedere", x: 11.339248, y: 50.965595 },
-  { name: "yulia", x: 11.306187, y: 50.980236 },
-  { name: "lucy", x: 12.327041, y: 51.327045 },
-  { name: "andreas", x: 9.484276, y: 51.312328 },
-  { name: "adrian", x: 13.44589, y: 52.442823 },
-  { name: "franzi", x: 8.79036, y: 53.085036 },
-  { name: "viola", x: 11.566735, y: 48.141888 },
-  { name: "konse", x: 13.760927, y: 51.05613 },
-  { name: "yael", x: 8.219497, y: 49.14952 },
-  { name: "parents", x: 4.949638, y: 52.343978 },
-  { name: "brother", x: 4.457034, y: 51.930167 },
-  { name: "dieuwertje", x: -4.24847, y: 55.856054 },
-  { name: "fennaGent", x: 3.692499, y: 51.060821 },
-  { name: "edo", x: 11.264115, y: 43.767498 },
-  { name: "gulsah", x: 29.020392, y: 41.05499 },
-  { name: "anja", x: 13.009123, y: 55.607879 },
-  { name: "fennaAzoia", x: -9.18201, y: 38.428754 },
-  { name: "penelope", x: -78.876708, y: 42.863777 },
-  { name: "claire", x: -118.384310, y: 34.003106 },
-  { name: "esther", x: -13.254891, y: 8.47137 },
-  { name: "jorge", x: -90.506725, y: 14.626069 },
-  { name: "alineSaoPaulo", x: -46.635193, y: -23.565544 },
-  { name: "grace", x: 123.903971, y: 10.299135 },
-  { name: "queenMaudLand", x: -149.070936, y: -79.519699 },
-    { name: "endOftheWorld", x: -176.618274, y: 0.811322 }, 
-  { name: "earthsOrigin", x: 0.0, y: 0.0 }
-];
+var friends = [{ name: "Your Colour", x: 0.0, y: 0.0 }];
 
 var places = [
   {
@@ -98,6 +65,7 @@ var yminMap;
 var ymaxMap;
 var xminMap;
 var xmaxMap;
+var maxDista;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -119,8 +87,9 @@ function setup() {
 
 function position_changed(pos)
 {
-  me.x = pos.coords.longitude;
-  me.y = pos.coords.latitude;
+  friends.x = pos.coords.longitude;
+  console.log(friends.x);
+  friends.y = pos.coords.latitude;
 }
 
 function nav_error(error)
@@ -128,16 +97,16 @@ function nav_error(error)
   switch(error.code)
   {
       case error.PERMISSION_DENIED:
-          x.innerHTML = "You denied access"
+          x.innerHTML = "You denied access";
           break;
       case error.POSITION_UNAVAILABLE:
-          x.innerHTML = "The position is unfortunately not available at this time"
+          x.innerHTML = "The position is unfortunately not available at this time";
           break;
       case error.TIMEOUT:
-          x.innerHTML = "Taking too long"
+          x.innerHTML = "Taking too long";
           break;
       case error.UNKNOWN_ERROR:
-          x.innerHTML = "Huh?!"
+          x.innerHTML = "Huh?!";
           break;
   }
 }
@@ -145,12 +114,14 @@ function nav_error(error)
 function draw() {
   //loc2X = friends[friend_index].x;
   //loc2Y = friends[friend_index].y;
-  loc1X = me[0].x;
+  loc1X = 15.041226;
   loc1Y = me[0].y;
 
+  loc2X = friends.x;
+  loc2Y = friends.y;
+
   for (var i=0; i<places.length; i++) {
-    loc2X = friends[friend_index].x;
-    loc2Y = friends[friend_index].y;
+
 
     if (
     loc2X < places[place_index].xmax &&
@@ -162,8 +133,9 @@ function draw() {
     xmaxMap = places[place_index].xmax;
     yminMap = places[place_index].ymin;
     ymaxMap = places[place_index].ymax;
-    maxDist = places[place_index].maxDist;
+    maxDista = places[place_index].maxDist;
   }
+  console.log(loc2X);
   }
 
   colorMode(HSB);
@@ -171,7 +143,7 @@ function draw() {
   middlepoint = getCendroid();
   colH = map(middlepoint, xminMap, ymaxMap, 0, 360);
   distan = getDistance();
-  colS = map(distan, 0, maxDist, 0, 100);
+  colS = map(distan, 0, maxDista, 0, 100);
   angleNum = getAngle();
   colB = map(angleNum, -360, 360, 0, 100);
   drawBackground();
@@ -233,7 +205,7 @@ function touchend(){
   loc2X = friends[friend_index].x;
   loc2Y = friends[friend_index].y;
   loc1X = me[0].x;
-  loc1Y = me[0].y;   
+  loc1Y = me[0].y;
 }
 
 function mouseClicked() {
